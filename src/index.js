05a5e3aaa8b64e45f99942ebf7256971ee4842da
LIU GUANG CHEN <boyislight@gmail.com>
2020-03-17 00:44:33 +0800

    feat: PEA setting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import * as serviceWorker from "./serviceWorker";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  // <React.StrictMode>
-  <App />,
-  // </React.StrictMode>,
-  rootElement
-);
+ReactDOM.render(<App />,rootElement);
+
+serviceWorker.register();
